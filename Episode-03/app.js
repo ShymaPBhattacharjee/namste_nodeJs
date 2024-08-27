903fbe4cf727e95a3f81ed04a6e8733b06d18e43
unknown <bhattacharjeeshyam17@gmail.com>
2024-08-27 20:54:41 +0530

    episode 3 - let's have the code

---
+++ Episode-03/app.js
@@ -0,0 +1,15 @@
+var courseName = "Namaste NodeJs";
+
+var a = 10;
+var b = 20;
+
+// console.log(courseName);
+// console.log(a + b);
+
+// console.log(global);
+
+console.log(this); // empty object {}
+
+console.log(globalThis);
+
+console.log(global === globalThis);
